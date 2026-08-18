e779c80996cec6df32efdfddde52c602f3cb870c
mayuriWagh2002 <mayuriwagh19072002@gmail.com>
2026-08-18 17:08:16 +0530

    Add GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+   base: "/Phronix_Team_Web/",
   plugins: [react()],
   server: {
     port: 5173,
