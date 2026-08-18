f27255ab3d0d095b4adf48a22cc1c34cf50c51a6
mayuriWagh2002 <mayuriwagh19072002@gmail.com>
2026-08-18 17:20:05 +0530

    Fix GitHub Pages routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "./styles/layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Phronix_Team_Web">
       <App />
     </BrowserRouter>
   </React.StrictMode>
