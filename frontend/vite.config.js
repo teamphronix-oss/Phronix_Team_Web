16e9795e89f37a7a70760349d576e95f573aa84c
pujapurkar <pujapurkar20@gmail.com>
2026-08-29 17:05:30 +0530

    updated vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-   base: "/Phronix_Team_Web/",
+   base: process.env.VERCEL ? "/" : "/Phronix_Team_Web/",
   plugins: [react()],
   server: {
     port: 5173,
