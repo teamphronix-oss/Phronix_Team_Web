b43c84c0f920d36b7f310c7dd10ea3e805f23565
pujapurkar <pujapurkar20@gmail.com>
2026-09-02 22:10:58 +0530

    Fix admin Vercel deployment

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,7 +2,8 @@
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Phronix_Admin_Web/",
   plugins: [react()],
-  server: { port: 5175 },
+  server: {
+    port: 5175,
+  },
 });
