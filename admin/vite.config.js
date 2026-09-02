1e8f124a6acddab2af31f2c816c9b35f7ca6e186
pujapurkar <pujapurkar20@gmail.com>
2026-09-02 22:19:38 +0530

    Fix admin Vercel base path

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,6 +2,7 @@
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.VERCEL ? "/" : "/Phronix_Admin_Web/",
   plugins: [react()],
   server: {
     port: 5175,
